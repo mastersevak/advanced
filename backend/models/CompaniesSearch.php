542dc99206e1eabb070a06f01b7325db21c6a856
Sevak Sargsyan <sevak085544@rambler.ru>
2015-07-11 01:11:41 +0400

    Yii2 Lesson - 32 Global Search for Companies

--- backend/models/CompaniesSearch.php
+++ backend/models/CompaniesSearch.php
@@ -10,61 +10,62 @@ use backend\models\Companies;
 /**
  * CompaniesSearch represents the model behind the search form about `backend\models\Companies`.
  */
-class CompaniesSearch extends Companies
-{
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['company_id'], 'integer'],
-            [['company_naem', 'company_email', 'company_addres', 'company_creates_date', 'company_status'], 'safe'],
-        ];
-    }
+class CompaniesSearch extends Companies {
 
-    /**
-     * @inheritdoc
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+	public $globalSearch;
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query = Companies::find();
+	/**
+	 * @inheritdoc
+	 */
+	public function rules() {
+		return [
+			[['company_id'], 'integer'],
+			[['globalSearch', 'company_naem', 'company_email', 'company_addres', 'company_creates_date', 'company_status'], 'safe'],
+		];
+	}
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
+	/**
+	 * @inheritdoc
+	 */
+	public function scenarios(){
+		// bypass scenarios() implementation in the parent class
+		return Model::scenarios();
+	}
 
-        $this->load($params);
+	/**
+	 * Creates data provider instance with search query applied
+	 *
+	 * @param array $params
+	 *
+	 * @return ActiveDataProvider
+	 */
+	public function search($params)
+	{
+		$query = Companies::find();
 
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
 
-        $query->andFilterWhere([
-            'company_id' => $this->company_id,
-            'company_creates_date' => $this->company_creates_date,
-        ]);
+		$this->load($params);
 
-        $query->andFilterWhere(['like', 'company_naem', $this->company_naem])
-            ->andFilterWhere(['like', 'company_email', $this->company_email])
-            ->andFilterWhere(['like', 'company_addres', $this->company_addres])
-            ->andFilterWhere(['like', 'company_status', $this->company_status]);
+		if (!$this->validate()) {
+			// uncomment the following line if you do not want to return any records when validation fails
+			// $query->where('0=1');
+			return $dataProvider;
+		}
+		// pakum enq globalSearch grelu hamar
+		// $query->andFilterWhere([
+		// 	'company_id' => $this->company_id,
+		// 	'company_creates_date' => $this->company_creates_date,
+		// ]);
 
-        return $dataProvider;
-    }
+		$query	->orFilterWhere(['like', 'company_naem',			$this->globalSearch])
+				->orFilterWhere(['like', 'company_email',			$this->globalSearch])
+				->orFilterWhere(['like', 'company_addres',			$this->globalSearch])
+				->orFilterWhere(['like', 'company_status',			$this->globalSearch])
+				->orFilterWhere(['like', 'company_creates_date',	$this->globalSearch]);
+
+		return $dataProvider;
+	}
 }
--- backend/views/companies/_search.php
+++ backend/views/companies/_search.php
@@ -15,17 +15,14 @@ use yii\widgets\ActiveForm;
 		'method' => 'get',
 	]); ?>
 
-	<?= $form->field($model, 'company_id') ?>
+	<?= $form->field($model, 'globalSearch') ?>
 
-	<?= $form->field($model, 'company_naem') ?>
-
-	<?= $form->field($model, 'company_email') ?>
-
-	<?= $form->field($model, 'company_addres') ?>
-
-	<?= $form->field($model, 'company_creates_date') ?>
-
-	<?= $form->field($model, 'company_status')->dropDownList(['active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => 'Status']) ?>
+	<? // =$form->field($model, 'company_id') ?>
+	<? // =$form->field($model, 'company_naem') ?>
+	<? // =$form->field($model, 'company_email') ?>
+	<? // =$form->field($model, 'company_addres') ?>
+	<? // =$form->field($model, 'company_creates_date') ?>
+	<? // =$form->field($model, 'company_status')->dropDownList(['active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => 'Status']) ?>
 
 	<div class="form-group">
 		<?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
