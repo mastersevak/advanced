91f86c59428487daefa965ba2ac93a53a9edd4a7
Sevak Sargsyan <sevak085544@rambler.ru>
2015-07-14 10:14:08 +0400

    vendor

---
+++ vendor/bariew/yii2-module-migration-controller/ModuleMigration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This is old class ith wrong naming
+ * It's saved only for compatibility
+ */
+namespace bariew\moduleMigration;
+
+
+class ModuleMigration extends ModuleMigrateController{}
